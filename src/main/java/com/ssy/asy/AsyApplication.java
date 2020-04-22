c30529b5e8227900ac69a56f6a60d73c70abff61
914371053@qq.com <914371053@qq.com>
2020-04-22 16:58:09 +0800

    测试

---
+++ src/main/java/com/ssy/asy/AsyApplication.java
@@ -0,0 +1,13 @@
+package com.ssy.asy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssy/asy/AsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssy.asy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
